52ffda064b367ac6207caffb1d4ccbb941fd9793
fakesanghyeok <s24066@gsm.hs.kr>
2024-08-29 23:09:59 +0900

    수정본1

---
+++ app.js
@@ -0,0 +1,44 @@
+function onGeoOk() {
+    const url = `https://port-0-tofaker-backend-lzsaeexf05f2c47e.sel4.cloudtype.app/api/v1/feed/random`;
+    fetch(url).then(response => response.json())
+    .then(data => {
+        console.log(data)
+        /*const idNumber = document.getElementById("id");
+        const contents = document.getElementById("content");
+        idNumber.innerText = data.id;
+        contents.innerText = data.title;
+        console.log(data.pictureUrl);
+
+
+        const Img = document.getElementById("emoji");
+        Img.onload = function() {
+           Img.width =Img.naturalWidth / 8;
+           Img.height =Img.naturalHeight / 8;
+        };
+        Img.src = data.pictureUrl;
+*/
+    });
+}
+
+onGeoOk();
+
+function submitForm() {
+    const form = document.getElementById('myForm');
+    const formData = new FormData(form);
+
+    console.log(formData)
+
+    const url = 'https://port-0-tofaker-backend-lzsaeexf05f2c47e.sel4.cloudtype.app/api/v1/feed'; // API의 URL로 변경하세요.
+
+    fetch(url, {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
